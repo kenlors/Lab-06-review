3a718cfc5d9fa8a3a076b143384d4192ef35fce2
Kenny <Kenny@Checkxs-MacBook-Air.local>
2020-10-14 17:02:59 -0400

    revising files

---
+++ bmi.py
@@ -0,0 +1,9 @@
+import sys
+
+print("BMI: Body Mass Index Calculator")
+userWeight = input("Enter your weight(in pounds):")
+userHeight =input("Enter your height (in inches):")
+
+bmi = (703 * float(userWeight)) / (float(userHeight) * float(userHeight))
+
+print("Your body mass index (BMI) is: " + str(bmi) + "%")
